47fd5973ce7099fd8710de95f339fd693d2de874
gaolei <gaoleigaodazhuang@126.com>
2024-11-18 22:11:42 +0800

    初始化项目代码

---
+++ src/test/java/com/study/lab1cicd/Lab1CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.lab1cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1CiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
