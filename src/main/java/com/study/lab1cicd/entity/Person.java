3e86b1802e87c2cd5b57ca9a5763536e1a216f71
gaolei <gaoleigaodazhuang@126.com>
2024-11-20 12:17:05 +0800

    初始化代码

--- src/main/java/com/study/lab1cicd/entity/Person.java
+++ src/main/java/com/study/lab1cicd/entity/Person.java
@@ -8,11 +8,18 @@ import jakarta.persistence.Id;
 @Entity
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name=name;
+        this.age=age;
+    }
 
     // Getters and Setters
 
